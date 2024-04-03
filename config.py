892a8cb284e90188d2de44c0b47580484aa0a4a9
Rafter148 <165582023+Rafter148@users.noreply.github.com>
2024-04-03 14:37:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 
-API_ID = API_ID = 25298674
+API_ID = API_ID = 28528007
 
-API_HASH = os.environ.get("API_HASH", "1a7e01278b2bf0fe975fb8ce7c5ead8d")
+API_HASH = os.environ.get("API_HASH", "38464da16c80310cabc8d13952419cf3")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6728485784:AAFHgTLL28oqvKWaO_ObX3a3oFIWruT_x54")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7048453277:AAGmt6yJFVp0diLHVYQcZObMRDoU_-TWUoA")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER", 6793357832))
+OWNER = int(os.environ.get("OWNER", 6717769514))
 
 LOG = -1002120460486
 
